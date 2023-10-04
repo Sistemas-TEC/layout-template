d4a5ddfe5f47bcde4fce9b74df7a0b87eff87f26
Maynor Martínez <81202099+Chacalerks@users.noreply.github.com>
2023-10-04 00:40:20 -0600

    Initial commit

---
+++ CancherksWebApp/Pages/RequesterUser/Reservation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CancherksWebApp.Pages.RequesterUser
+{
+    public class ReservationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Cancherks
+Sport installation system where students and employees can reserve any type of sport installation during the available times. 
+The administrators can manage the requests.
+
+Authors:
+Maynor Martínez and Fernanda Murillo.
